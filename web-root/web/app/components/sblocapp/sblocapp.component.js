b604a2e46352182b1e7689106b711d77b37a0aa2
sumit kumar <sumit kumar>
2016-09-28 23:32:10 +0530

    commiting files after adding closures

--- web-root/web/app/components/sblocapp/sblocapp.component.js
+++ web-root/web/app/components/sblocapp/sblocapp.component.js
@@ -1,35 +1,36 @@
 'use strict';
-var sblocappController = function($router) {
-	console.log("ROUTER SBLOC APP: ", $router);
-};
 
-sblocappController.$inject = ['$router'];
+(function() {
 
-var componentConfig = {
-	// isolated scope binding
-    bindings: {
-        message: '<'
-    },
-	templateUrl: 'sblocapp/sblocapp.html',
-	controller: sblocappController,
-	$routeConfig: [
-		{
-			path: '/loandetails',
-			name: 'LoanDetails',
-			component: 'loanDetails'
-		},
-		{
-			path: '/loanlisting',
-			name: 'LoanListing',
-			component: 'loanListing'
-		},
-		{
-			path: '/login',
-			name: 'Login',
-			component: 'login'
-		}
-	]
-};
+    var sblocappController = function($router) {
+        console.log("ROUTER SBLOC APP: ", $router);
+    };
 
-module.exports = angular.module('sblocapp')
-.component('sblocApp', componentConfig);
+    sblocappController.$inject = ['$router'];
+
+    var componentConfig = {
+        // isolated scope binding
+        bindings: {
+            message: '<'
+        },
+        templateUrl: 'sblocapp/sblocapp.html',
+        controller: sblocappController,
+        $routeConfig: [{
+            path: '/loandetails',
+            name: 'LoanDetails',
+            component: 'loanDetails'
+        }, {
+            path: '/loanlisting',
+            name: 'LoanListing',
+            component: 'loanListing'
+        }, {
+            path: '/login',
+            name: 'Login',
+            component: 'login'
+        }]
+    };
+
+    module.exports = angular.module('sblocapp')
+        .component('sblocApp', componentConfig);
+
+})();
